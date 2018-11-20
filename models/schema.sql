b45b5a533a65f6ab15f03ed51a7959db59bfbfe2
jlrt37 <joshua.l.ragon@gmail.com>
2018-11-19 19:59:39 -0600

    edits to schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,20 +1,17 @@
-DROP DATABASE IF EXISTS a3t7g6zh8ynzwgit;
-CREATE DATABASE a3t7g6zh8ynzwgit;
+USE a3t7g6zh8ynzwgit;
 
-USE garage_sailor;
-
-CREATE TABLE buyers (
-	item_id INTEGER(11) NOT NULL,
-	addresses VARCHAR(20) NOT NULL,
-	PRIMARY KEY (item_id)
-);
-    
 CREATE TABLE sellers (
-    address_ID INT NOT NULL,
-    address VARCHAR(100) NOT NULL,
+    sellers_id INT NOT NULL,
+    address_id VARCHAR(100) NOT NULL,
     products VARCHAR(100) NOT NULL,
-    costs DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY(address_ID));
+    date_id DATE NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(sellers_id));
 
-INSERT INTO Departments(address, products, cost)
-VALUES (",", ",", ",");
+CREATE TABLE buyers_wish_list (
+    buyers_wish_list_id INT NOT NULL,
+    name_id VARCHAR (100) NOT NULL,
+    products VARCHAR(100) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY (buyers_wish_list_id)
+)
--- models/seeds.sql
+++ models/seeds.sql
@@ -1,3 +1,6 @@
-INSERT INTO sellers (address, products, costs) VALUES ('1100 Jackson Ct, Liberty, MO 64068', 'clothes, electronics, treadmill', 5.00, false);
-INSERT INTO sellers (address, products, costs) VALUES ('19040 E Valley View Pkwy, Independence, MO 64055', 'dvds, toys, hulahoops', 50.00, false);
-INSERT INTO sellers (address, products, costs) VALUES ('', 'pencils, clothes, books', 9500.00, false);
+INSERT INTO sellers (sellers_id, address_id, products, costs) VALUES ('Jay Adams', '1100 Jackson Ct, Liberty, MO 64068', 'clothes, electronics, treadmill', 5.00, false);
+INSERT INTO sellers (sellers_id, address_id, products, costs) VALUES ('Rick Deckard', '19040 E Valley View Pkwy, Independence, MO 64055', 'dvds, toys, hulahoops', 50.00, false);
+INSERT INTO sellers (sellers_id, address_id, products, costs) VALUES ('Jim Sinegal', '12600 S Quivira Rd, Overland Park, KS 66213', 'pencils, clothes, books', 9500.00, true);
+
+INSERT INTO buyers_wish_list (name_id, products) VALUES ("Jay Adams", '"clothes", "dvds","books"', true);
+INSERT INTO buyers_wish_list (name_id, products) VALUES ("Rick Deckard", '"electronics", "outdoors items", "kitchen utensils"', false);
