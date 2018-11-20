83a7fe49ae42e95c5d9af0e8308c56f6f3bb6b89
jlrt37 <joshua.l.ragon@gmail.com>
2018-11-19 19:38:17 -0600

    seeds/schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS a3t7g6zh8ynzwgit;
+CREATE DATABASE a3t7g6zh8ynzwgit;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE garage_sailor;
+
+CREATE TABLE buyers (
+	item_id INTEGER(11) NOT NULL,
+	addresses VARCHAR(20) NOT NULL,
+	PRIMARY KEY (item_id)
+);
+    
+CREATE TABLE sellers (
+    address_ID INT NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    products VARCHAR(100) NOT NULL,
+    costs DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY(address_ID));
+
+INSERT INTO Departments(address, products, cost)
+VALUES (",", ",", ",");
---
+++ models/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO sellers (address, products, costs) VALUES ('1100 Jackson Ct, Liberty, MO 64068', 'clothes, electronics, treadmill', 5.00, false);
+INSERT INTO sellers (address, products, costs) VALUES ('19040 E Valley View Pkwy, Independence, MO 64055', 'dvds, toys, hulahoops', 50.00, false);
+INSERT INTO sellers (address, products, costs) VALUES ('', 'pencils, clothes, books', 9500.00, false);
