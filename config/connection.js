fa253e243e19acc2ebedcf7cd1d560517380be1f
Dustin Watkins <watkins656@gmail.com>
2018-11-19 19:07:17 -0600

    connection orm

---
+++ config/connection.js
@@ -0,0 +1,32 @@
+let dotenv = require('dotenv').config({ path: __dirname + '/../.env' })
+
+
+let mySQLPassword = process.env.MYSQL_PASSWORD;
+
+// Set up MySQL connection.
+var mysql = require("mysql");
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: mySQLPassword,
+    database: "a3t7g6zh8ynzwgit"
+  });
+};
+  // Make connection.
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+  // Export connection for our ORM to use.
+  module.exports = connection;
