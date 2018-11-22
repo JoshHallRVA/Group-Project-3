3f94b990d93cc4484bbfda9f14fed41d362b2cb2
mjbenefiel <michaeljbenefiel@gmail.com>
2018-11-22 10:00:14 -0600

    11222018 commit 10:00am buyer seller pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -29,6 +29,15 @@ module.exports = function(app) {
     });
   });
 
+       app.get("/seller", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("seller", {
+        msg: "LOGO",
+        examples: dbExamples
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
@@ -37,6 +46,8 @@ module.exports = function(app) {
       });
     });
   });
+
+  
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
