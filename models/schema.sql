82358b4fe8316c4c6874554276269abd4568f133
mjbenefiel <michaeljbenefiel@gmail.com>
2018-11-21 20:58:08 -0600

    11212018 commit 8:57pm frontend hbrs full template working

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+USE a3t7g6zh8ynzwgit;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE sellers (
+    sellers_id INT NOT NULL,
+    address_id VARCHAR(100) NOT NULL,
+    products VARCHAR(100) NOT NULL,
+    date_id DATE NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(sellers_id));
+
+CREATE TABLE buyers_wish_list (
+    buyers_wish_list_id INT NOT NULL,
+    name_id VARCHAR (100) NOT NULL,
+    products VARCHAR(100) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY (buyers_wish_list_id)
+)
